8c16ccff9362ac0a486a0b86f426d586c29bd170
Gustavo Valle <gustavo.vallerp@hotmail.com>
2022-06-19 11:23:01 -0300

    change name of system

--- src/main/java/com/gustavovalle/medicalappointment/MedicalAppointmentApplication.java
+++ src/main/java/com/gustavovalle/medicalappointment/MedicalAppointmentApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CustomAuthenticationApplication {
+public class MedicalAppointmentApplication {
 
     public static void main (String[] args) {
-        SpringApplication.run(CustomAuthenticationApplication.class, args);
+        SpringApplication.run(MedicalAppointmentApplication.class, args);
     }
 }
